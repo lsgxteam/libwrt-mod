openwrt/openwrt

commit de94587e709e6d8ddaca4fe534e776d2e87437df

mediatek: filogic: don't rely on image preset in flash or sysupgrade
Instead of trying to figure out the actual root device, just use the
kernel 'root' cmdline parameter as a hint to decide which device to
flash to.

Signed-off-by: Daniel Golle <daniel@makrotopia.org>

target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
@@ -2,23 +2,20 @@ REQUIRE_IMAGE_METADATA=1
 
 platform_do_upgrade() {
 	local board=$(board_name)
-	local file_type=$(identify $1)
 
 	case "$board" in
 	bananapi,bpi-r3)
-		export_bootdevice
-		export_partdevice rootdev 0
-		case "$rootdev" in
-		mmc*)
+		case "$(cmdline_get_var root)" in
+		/dev/mmc*)
 			CI_ROOTDEV="$rootdev"
 			CI_KERNPART="production"
 			emmc_do_upgrade "$1"
 			;;
-		mtdblock*)
+		/dev/mtdblock*)
 			PART_NAME="fit"
 			default_do_upgrade "$1"
 			;;
-		ubiblock*)
+		/dev/ubiblock*)
 			CI_KERNPART="fit"
 			nand_do_upgrade "$1"
 			;;
@@ -58,10 +55,8 @@ platform_check_image() {
 platform_copy_config() {
 	case "$(board_name)" in
 	bananapi,bpi-r3)
-		export_bootdevice
-		export_partdevice rootdev 0
-		case "$rootdev" in
-		mmc*)
+		case "$(cmdline_get_var root)" in
+		/dev/mmc*)
 			emmc_copy_config
 			;;
 		esac

lsgxteam/libwrt-mod

commit 134114f9745cd85520b9fa04a1685f8f640d011a

mediatek: filogic: fix sysupgrade on MMC on the BPi-R3
A previous attempt to simplify things went wrong and now sysupgrade
is broken on this device. Fix that.

Fixes: de94587e70 ("mediatek: filogic: don't rely on image preset in flash or sysupgrade")
Signed-off-by: Daniel Golle <daniel@makrotopia.org>

target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
@@ -43,17 +43,20 @@ platform_do_upgrade() {
 
 	case "$board" in
 	bananapi,bpi-r3)
-		case "$(cmdline_get_var root)" in
-		/dev/mmc*)
+		local rootdev="$(cmdline_get_var root)"
+		rootdev="${rootdev##*/}"
+		rootdev="${rootdev%p[0-9]*}"
+		case "$rootdev" in
+		mmc*)
 			CI_ROOTDEV="$rootdev"
 			CI_KERNPART="production"
 			emmc_do_upgrade "$1"
 			;;
-		/dev/mtdblock*)
+		mtdblock*)
 			PART_NAME="fit"
 			default_do_upgrade "$1"
 			;;
-		/dev/ubiblock*)
+		ubiblock*)
 			CI_KERNPART="fit"
 			nand_do_upgrade "$1"
 			;;