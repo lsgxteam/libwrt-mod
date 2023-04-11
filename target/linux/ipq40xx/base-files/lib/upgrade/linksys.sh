openwrt/openwrt

commit 8634c1080d5033e8cf0069ee7447821726232b95

ipq40xx: Fix Linksys upgrade, restore config step
It appears that the refactor of the upgrade process for NAND devices
resulted in the nand_do_upgrade_success step not being called for
devices using the linksys.sh script. As a result, configuration
was not preserved over sysupgrade steps.

This was restored for some devices in
commit 84ff6c90dda1 ("base-files: bring back nand_do_upgrade_success").
This restored preservation of config for ipq40xx devices using the
linksys.sh script. Other devices and targets have not been examined.

Closes: #11677
Fixes: e25e6d8e54 ("base-files: fix and clean up nand sysupgrade code")
Tested-on: EA8300

Signed-off-by: Jeff Kletsky <git-commits@allycomm.com>
(checkpatch nitpick)
Signed-off-by: Christian Lamparter <chunkeey@gmail.com>

target/linux/ipq40xx/base-files/lib/upgrade/linksys.sh
@@ -100,7 +100,12 @@ platform_do_upgrade_linksys() {
 		fi
 
 		# complete std upgrade
-		nand_upgrade_tar "$1"
+		if nand_upgrade_tar "$1" ; then
+			nand_do_upgrade_success
+		else
+			nand_do_upgrade_failure
+		fi
+
 	}
 
 	[ "$magic_long" = "27051956" ] && {

lsgxteam/libwrt-mod

commit 3b9f66da43549ff7db0597d29a23396ee7859d2b

base-files: fix Linksys upgrade, restore config step
It appears that the refactor of the upgrade process for NAND devices                                                                                                                            resulted in the nand_do_upgrade_success step not being called for
devices using the linksys.sh script. As a result, configuration was
not preserved over sysupgrade steps.

This corrects a typo in the call of nand_do_upgrade_failed for ipq40xx
and ipq806x devices using the linksys.sh script.

Fixes: 8634c1080d50 ("ipq40xx: Fix Linksys upgrade, restore config step")
Fixes: 2715aff5df83 ("ipq806x: Fix Linksys upgrade, restore config step")
Signed-off-by: Michael Trinidad <trinidude4@hotmail.com>

target/linux/ipq40xx/base-files/lib/upgrade/linksys.sh
@@ -103,7 +103,7 @@ platform_do_upgrade_linksys() {
 		if nand_upgrade_tar "$1" ; then
 			nand_do_upgrade_success
 		else
-			nand_do_upgrade_failure
+			nand_do_upgrade_failed
 		fi
 
 	}

target/linux/ipq806x/base-files/lib/upgrade/linksys.sh
@@ -97,7 +97,7 @@ platform_do_upgrade_linksys() {
 		if nand_upgrade_tar "$1" ; then
 			nand_do_upgrade_success
 		else
-			nand_do_upgrade_failure
+			nand_do_upgrade_failed
 		fi
 
 	}