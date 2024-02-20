openwrt/openwrt

commit be9023ed4311c89fedad17c9d8b821828565903e

build: fix opkg flags in rootfs
By default opkg sets the "user" flag when a package is installed,
which resulted in most packages in the rootfs having this flag
set incorrectly. This patch removes the "user" flag from all
installed packages when preparing the rootfs image.

Fixes: #14427
Signed-off-by: Justin Klaassen <justin@tidylabs.app>

include/rootfs.mk
@@ -87,6 +87,11 @@ define prepare_rootfs
 			fi; \
 		done || true \
 	)
+	awk -i inplace \
+		'/^Status:/ { \
+			if ($$3 == "user") { $$3 = "ok" } \
+			else { sub(/,\<user\>|\<user\>,/, "", $$3) } \
+		}1' $(1)/usr/lib/opkg/status
 	$(if $(SOURCE_DATE_EPOCH),sed -i "s/Installed-Time: .*/Installed-Time: $(SOURCE_DATE_EPOCH)/" $(1)/usr/lib/opkg/status)
 	@-find $(1) -name CVS -o -name .svn -o -name .git -o -name '.#*' | $(XARGS) rm -rf
 	rm -rf \

lsgxteam/libwrt-mod

commit 40238852c2a7471ec1ce322f27113a6701c69bd0

build: don't drop 'user' flag when using the ImageBuilder
Commit be9023ed43 ("build: fix opkg flags in rootfs") introduced a
call to 'awk' which removes the 'user' flag from all installed
packages in the opkg status file. While is is somehow desireable when
building images directly within the buildroot, when using the
ImageBuilder dropping the 'user' flag means loosing information about
a package being deliberately selected or just implicitely pulled as a
dependency. And that then break tools like 'auc' which request only
packages having the 'user' flag from the asu server, resulting in
broken images being delivered to users.

Restore the original behavior in case of an image being created using
the ImageBuilder.

Fixes: be9023ed43 ("build: fix opkg flags in rootfs")
Signed-off-by: Daniel Golle <daniel@makrotopia.org>

include/rootfs.mk
@@ -87,11 +87,11 @@ define prepare_rootfs
 			fi; \
 		done || true \
 	)
-	awk -i inplace \
+	$(if $(IB),,awk -i inplace \
 		'/^Status:/ { \
 			if ($$3 == "user") { $$3 = "ok" } \
 			else { sub(/,\<user\>|\<user\>,/, "", $$3) } \
-		}1' $(1)/usr/lib/opkg/status
+		}1' $(1)/usr/lib/opkg/status)
 	$(if $(SOURCE_DATE_EPOCH),sed -i "s/Installed-Time: .*/Installed-Time: $(SOURCE_DATE_EPOCH)/" $(1)/usr/lib/opkg/status)
 	@-find $(1) -name CVS -o -name .svn -o -name .git -o -name '.#*' | $(XARGS) rm -rf
 	rm -rf \